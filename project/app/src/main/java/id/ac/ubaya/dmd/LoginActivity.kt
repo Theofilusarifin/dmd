a4575d6744e7d2db86a49605e1726a6142f1c8e4
Theofilusarifin <theofilusarifin@gmail.com>
2022-12-28 22:15:02 +0700

    Front End Auth

---
+++ project/app/src/main/java/id/ac/ubaya/dmd/LoginActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.ubaya.dmd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
