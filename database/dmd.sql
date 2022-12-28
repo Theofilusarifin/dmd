720abad69f02ca9c66dc75df96254972e59e06bd
Snahfu <hanxin131@gmail.com>
2022-12-28 18:53:06 +0700

    update sql
    done

--- database/dmd.sql
+++ database/dmd.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 18, 2022 at 01:47 PM
+-- Generation Time: Dec 28, 2022 at 12:52 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -32,7 +32,29 @@ CREATE TABLE `comments` (
   `user_id` int(11) NOT NULL,
   `meme_id` int(11) NOT NULL,
   `content` text NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `user_id` int(11) NOT NULL,
+  `meme_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `like_comments`
+--
+
+CREATE TABLE `like_comments` (
+  `user_id` int(11) NOT NULL,
+  `comment_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -44,10 +66,28 @@ CREATE TABLE `comments` (
 CREATE TABLE `memes` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `url_img` varchar(45) NOT NULL,
+  `url_img` varchar(255) NOT NULL,
+  `creation_date` varchar(60) NOT NULL,
   `top_text` varchar(45) NOT NULL,
-  `bottom_text` varchar(45) NOT NULL,
-  `like_num` int(11) NOT NULL
+  `bottom_text` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `memes`
+--
+
+INSERT INTO `memes` (`id`, `user_id`, `url_img`, `creation_date`, `top_text`, `bottom_text`) VALUES
+(1, 1, 'https://www.generatormix.com/images/meme/stahp.jpg', '2022-12-25 00:48:00', 'Coba coba', 'Hai Hai');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reports`
+--
+
+CREATE TABLE `reports` (
+  `user_id` int(11) NOT NULL,
+  `meme_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -68,6 +108,14 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `first_name`, `last_name`, `password`, `registration_date`, `url_img`, `privacy_setting`) VALUES
+(1, 'Snahfu', 'Hans', 'Wirjawan', '12345678', '2022-12-25 14:15:10', 'http://pm1.narvii.com/5734/a7a08ce9ba236846588115fdd09f3f8d733d0850_00.jpg', 0),
+(2, 'Arifin', 'Theofilus', 'Arifin', '12345678', '2022-12-25 14:15:10', 'http://pm1.narvii.com/5734/a7a08ce9ba236846588115fdd09f3f8d733d0850_00.jpg', 0);
+
+--
 -- Indexes for dumped tables
 --
 
@@ -80,11 +128,35 @@ ALTER TABLE `comments`
   ADD KEY `fk_comments_memes1_idx` (`meme_id`);
 
 --
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`user_id`,`meme_id`),
+  ADD KEY `fk_users_has_memes_memes2_idx` (`meme_id`),
+  ADD KEY `fk_users_has_memes_users2_idx` (`user_id`);
+
+--
+-- Indexes for table `like_comments`
+--
+ALTER TABLE `like_comments`
+  ADD PRIMARY KEY (`user_id`,`comment_id`),
+  ADD KEY `fk_users_has_comments_comments1_idx` (`comment_id`),
+  ADD KEY `fk_users_has_comments_users1_idx` (`user_id`);
+
+--
 -- Indexes for table `memes`
 --
 ALTER TABLE `memes`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_memes_users_idx` (`user_id`);
+
+--
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
+  ADD PRIMARY KEY (`user_id`,`meme_id`),
+  ADD KEY `fk_users_has_memes_memes1_idx` (`meme_id`),
+  ADD KEY `fk_users_has_memes_users1_idx` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -106,13 +178,13 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT for table `memes`
 --
 ALTER TABLE `memes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -126,10 +198,31 @@ ALTER TABLE `comments`
   ADD CONSTRAINT `fk_comments_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
+-- Constraints for table `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `fk_users_has_memes_memes2` FOREIGN KEY (`meme_id`) REFERENCES `memes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_users_has_memes_users2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `like_comments`
+--
+ALTER TABLE `like_comments`
+  ADD CONSTRAINT `fk_users_has_comments_comments1` FOREIGN KEY (`comment_id`) REFERENCES `comments` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_users_has_comments_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
 -- Constraints for table `memes`
 --
 ALTER TABLE `memes`
   ADD CONSTRAINT `fk_memes_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `reports`
+--
+ALTER TABLE `reports`
+  ADD CONSTRAINT `fk_users_has_memes_memes1` FOREIGN KEY (`meme_id`) REFERENCES `memes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_users_has_memes_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
