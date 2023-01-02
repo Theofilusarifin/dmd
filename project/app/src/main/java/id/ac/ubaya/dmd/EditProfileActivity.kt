27400aa0624f1e916804faa206473bd9e94d3ea0
Snahfu <hanxin131@gmail.com>
2023-01-02 08:17:18 +0700

    update image from gallery or camera

--- project/app/src/main/java/id/ac/ubaya/dmd/EditProfileActivity.kt
+++ project/app/src/main/java/id/ac/ubaya/dmd/EditProfileActivity.kt
@@ -1,17 +1,29 @@
 package id.ac.ubaya.dmd
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.squareup.picasso.Picasso
+import com.theartofdev.edmodo.cropper.CropImage
 import kotlinx.android.synthetic.main.activity_edit_profile.*
+import kotlinx.android.synthetic.main.user_meme_card.view.*
 import org.json.JSONObject
 
+
 class EditProfileActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
@@ -87,6 +99,75 @@ class EditProfileActivity : AppCompatActivity() {
 //        Button back
         btnEditProfileBack.setOnClickListener{
             finish()
+        }
+
+//        Image View Change
+        imagePhotoProfile.setOnClickListener{
+            var pick:Boolean = true
+            if(pick == true){
+                // Select Image dari Camera
+                if(!checkCameraPermission()){
+                    requestCameraPermission();
+
+                }else PickImage();
+            }
+            else{
+                // Select Image dari Gallery
+                if(!checkGalleryPermission()){
+                    requestGalleryPermission();
+
+                }else PickImage();
+            }
+        }
+    }
+
+    private fun PickImage() {
+        CropImage.activity().start(this)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun requestCameraPermission() {
+        var arrayString = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        requestPermissions(
+            arrayString,
+            100
+        )
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun requestGalleryPermission() {
+        var arrayString = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        requestPermissions(
+            arrayString,
+            100
+        )
+    }
+
+    private fun checkCameraPermission(): Boolean {
+        // Minta ijin buka camera
+        var res1 = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+        // Minta ijin buat edit foto
+        var res2 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+        // res1 dan res2 harus TRUE
+        return res1 && res2;
+    }
+
+    private fun checkGalleryPermission(): Boolean {
+        // Minta ijin buat edit foto
+        var res2 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+        return res2;
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            val result = CropImage.getActivityResult(data)
+            if (resultCode == RESULT_OK) {
+                val resultUri: Uri = result.uri
+
+                Picasso.get().load(resultUri).into(imagePhotoProfile)
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                val error = result.error
+            }
         }
     }
 }
