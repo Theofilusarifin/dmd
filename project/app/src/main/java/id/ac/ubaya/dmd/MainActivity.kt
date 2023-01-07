1f2a481064caf3c7a1e3499500bcfc1d4993bdc2
Theofilusarifin <theofilusarifin@gmail.com>
2023-01-07 23:00:55 +0700

    Drawer Navigation

--- project/app/src/main/java/id/ac/ubaya/dmd/MainActivity.kt
+++ project/app/src/main/java/id/ac/ubaya/dmd/MainActivity.kt
@@ -1,21 +1,21 @@
 package id.ac.ubaya.dmd
 
-import android.content.Context
-import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.drawer_header.view.*
 import kotlinx.android.synthetic.main.drawer_layout.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     // Buat list fragment
     val fragments: ArrayList<Fragment> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,22 +29,9 @@ class MainActivity : AppCompatActivity() {
         drawerToggle.isDrawerIndicatorEnabled = true
         drawerToggle.syncState()
 
+        navView.setNavigationItemSelectedListener(this)
+
         supportActionBar?.title = ""
-
-        navView.setNavigationItemSelectedListener {
-            when(it.itemId) {
-                R.id.itemHome -> Toast.makeText(this, "Premium", Toast.LENGTH_SHORT).show()
-                R.id.itemMyCreation -> Toast.makeText(this, "Chart", Toast.LENGTH_SHORT).show()
-                R.id.itemLeaderbord -> Toast.makeText(this, "Playlist", Toast.LENGTH_SHORT).show()
-                R.id.itemSettings -> Toast.makeText(this, "Favourites", Toast.LENGTH_SHORT).show()
-            }
-            drawerLayout.closeDrawer(GravityCompat.START)
-            true
-        }
-
-        navView.fabMenuLogout.setOnClickListener {
-            Toast.makeText(this, "Favourites", Toast.LENGTH_SHORT).show()
-        }
 
         // ViewPager Process
         // Tambahin fragment ke listnya
@@ -75,9 +62,34 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+
+//        Set Default yang aktif adalah home
+        navView.getMenu().getItem(0).setChecked(true)
     }
 
     override fun onBackPressed() {
         Toast.makeText(this, "You cannot go back", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        val id = item.itemId
+
+        if (id == R.id.itemHome) {
+            rvListOfComment.currentItem = 0
+        }
+        else if (id == R.id.itemMyCreation) {
+            rvListOfComment.currentItem = 1
+        }
+        else if (id == R.id.itemLeaderbord) {
+            rvListOfComment.currentItem = 2
+        }
+        else if (id == R.id.itemSettings) {
+            rvListOfComment.currentItem = 3
+        }
+        navView.getMenu().getItem(rvListOfComment.currentItem).setChecked(true)
+        val drawer = findViewById<DrawerLayout>(R.id.drawerLayout)
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
