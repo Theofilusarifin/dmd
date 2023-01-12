e289bd746311b0ad8105a543015294d0f2ae260c
Theofilusarifin <theofilusarifin@gmail.com>
2023-01-12 08:54:59 +0700

    Update get_leaderboard.php

--- api/api_server/get_leaderboard.php
+++ api/api_server/get_leaderboard.php
@@ -11,24 +11,42 @@ $status = 'error';
 $msg = 'Get leaderboard error!';
 $leaderboards = [];
 
-// Get leaderboard detail
-$sql = "SELECT u.*, count(l.meme_id) as total_like FROM users u LEFT JOIN likes l on u.id = l.user_id LEFT JOIN memes m on m.id = l.meme_id GROUP BY u.id ORDER BY total_like DESC";
+// Get users detail
+$sql = "SELECT * FROM users";
 $stmt = $mysqli->prepare($sql);
 $stmt->execute();
 $res = $stmt->get_result();
 
+$users = [];
 while ($row = $res->fetch_assoc()) {
-    // Store leaderboard details to array
-    $leaderboard = array(
+    $user = array(
         "id" => $row['id'],
         "first_name" => $row['first_name'],
         "last_name" => $row['last_name'],
         "url_img" => $row['url_img'],
         "privacy_setting" => $row['privacy_setting'],
-        "total_like" => $row['total_like'],
+        "total_like" => 0,
     );
-    $leaderboards[] = $leaderboard;
+
+    $users[] = $user;
 }
+
+if (count($users) > 0){
+    // Get leaderboard detail
+    $sql = "SELECT m.user_id as user_id, count(l.user_id) as total_like FROM `likes` l INNER JOIN memes m on l.meme_id = m.id GROUP BY m.user_id";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->execute();
+    $res = $stmt->get_result();
+
+    while ($row = $res->fetch_assoc()) {
+        // Insert total like into detail users
+        $users[array_search($row['user_id'], array_column($users, 'id'))]['total_like'] = $row['total_like'];
+    }
+}
+
+// Sort Array
+array_multisort(array_column($users, "total_like"), SORT_DESC, $users);
+
 // Set success status 
 $status = 'success';
 $msg = "Get leaderboard successful!";
@@ -37,5 +55,5 @@ $msg = "Get leaderboard successful!";
 echo json_encode(array(
     "status" => $status,
     "msg" => $msg,
-    "leaderboards" => $leaderboards,
+    "leaderboards" => $users,
 ));
